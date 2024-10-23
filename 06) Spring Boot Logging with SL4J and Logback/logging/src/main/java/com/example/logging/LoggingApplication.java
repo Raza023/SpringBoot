e065492954c162a4276ac3f5218b65073acac6af
Hassan <hr770735@gmail.com>
2024-10-23 08:19:29 +0500

    logging done

--- 06) Spring Boot Logging with SL4J and Logback/logging/src/main/java/com/example/logging/LoggingApplication.java
+++ 06) Spring Boot Logging with SL4J and Logback/logging/src/main/java/com/example/logging/LoggingApplication.java
@@ -1,17 +1,44 @@
 package com.example.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class LoggingApplication {
 
-	
+    //In this tutorial we learnt three ways for logging:
+    //1) Using application.properties (it creates api.log and logs on console)
+    //2) Using application.yaml (it creates api.log and logs on console)
+    //3) using logback.xml (it creates logs/debug.xml and logs/error.log and logs on console)
+    Logger log = LoggerFactory.getLogger(LoggingApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoggingApplication.class, args);
-	}
+    @GetMapping("/test/{name}")
+    public String greeting(@PathVariable String name) {
+        String response = "";
+        try {
+            if (name.equalsIgnoreCase("test")) {
+                throw new RuntimeException(name+" is not a name...");
+            }
+            log.debug("Request {}", name);
+            response = "Hi " + name + "! Welcome to Lahore.";
+            log.debug("Response {}", response);
+        } catch (Exception e) {
+            log.error("An exception occurred", e);
+            response = "Error: " + e.getMessage();  // Return the exception message
+        } finally {
+            response = "An exception occurred: "+response;
+        }
+        return response;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingApplication.class, args);
+    }
 
 }
