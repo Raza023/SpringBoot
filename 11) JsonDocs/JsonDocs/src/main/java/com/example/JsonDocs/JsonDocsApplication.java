c0b63218f1b885878f602044e24e1b2952e9711c
Hassan <hr770735@gmail.com>
2024-12-11 22:48:24 +0500

    minor change

---
+++ 11) JsonDocs/JsonDocs/src/main/java/com/example/JsonDocs/JsonDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.JsonDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDocsApplication.class, args);
+	}
+
+}
---
+++ 11) JsonDocs/JsonDocs/src/test/java/com/example/JsonDocs/JsonDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JsonDocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
