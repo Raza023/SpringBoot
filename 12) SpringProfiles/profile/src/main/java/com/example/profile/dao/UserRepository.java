8a281e03f6be795c041bccaad07fdaff4d141c48
Hassan Raza <imhraza023@gmail.com>
2025-02-17 23:59:59 +0500

    message

--- 12) SpringProfiles/profile/src/main/java/com/example/profile/dao/UserRepository.java
+++ 12) SpringProfiles/profile/src/main/java/com/example/profile/dao/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     @Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:name)")
     User findByName(@Param("name") String name);
 
