b1c554f34888c5bdeca273d519aeb59c56d6aaa0
Hassan Raza <hassan.raza@redmath.com>
2025-07-07 17:41:14 +0500

    minor changes

--- 10 Spring Boot with swagger/swagger/src/main/java/com/example/swagger/controller/BookController.java
+++ 10 Spring Boot with swagger/swagger/src/main/java/com/example/swagger/controller/BookController.java
@@ -6,10 +6,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
@@ -38,7 +44,7 @@ public class BookController {
     )
     @GetMapping("/get/{bookId}")
     public Book fetchBookById(@PathVariable @Parameter(description = "ID of the book to be retrieved", required = true)
-                          int bookId) {
+    int bookId) {
         return service.getBook(bookId).orElseGet(() -> {
             System.out.println("I am getting book with id...." + bookId);
             return null;
