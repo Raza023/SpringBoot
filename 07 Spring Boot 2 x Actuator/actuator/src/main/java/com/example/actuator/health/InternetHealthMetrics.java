6132edb291a9c3c256120111c0952a1630e8af78
Hassan Raza <hassan.raza@redmath.com>
2025-07-07 10:47:08 +0500

    minor changes

--- 07 Spring Boot 2 x Actuator/actuator/src/main/java/com/example/actuator/health/InternetHealthMetrics.java
+++ 07 Spring Boot 2 x Actuator/actuator/src/main/java/com/example/actuator/health/InternetHealthMetrics.java
@@ -4,11 +4,12 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 
 @Component
-public class InternetHealthMatrics implements HealthIndicator {
+public class InternetHealthMetrics implements HealthIndicator {
     @Override
     public Health health() {
         return checkInternet() == true ? Health.up().withDetail("success code", "Active Internet Connection").build()
@@ -18,7 +19,8 @@ public class InternetHealthMatrics implements HealthIndicator {
     private boolean checkInternet() {
         boolean flag = false;
         try {
-            URL url = new URL("https://www.google.com");
+            URI uri = new URI("https://www.google.com");
+            URL url = uri.toURL();
             URLConnection connection = url.openConnection();
             connection.connect();
             flag = true;
