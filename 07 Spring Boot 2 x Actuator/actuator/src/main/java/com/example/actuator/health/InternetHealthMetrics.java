631c477463567103c8122944799a78f7ff66b338
Hassan Raza <hassan.raza@redmath.com>
2025-07-07 11:03:49 +0500

    minor changes

--- 07 Spring Boot 2 x Actuator/actuator/src/main/java/com/example/actuator/health/InternetHealthMetrics.java
+++ 07 Spring Boot 2 x Actuator/actuator/src/main/java/com/example/actuator/health/InternetHealthMetrics.java
@@ -8,6 +8,17 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 
+// The health() method is called by Spring Boot Actuator when the /actuator/health endpoint is accessed.
+// It checks the internet connection by trying to connect to a well-known URL (in this case, Google).
+// If the connection is successful, it returns an "up" status; otherwise, it returns a "down" status.
+
+// Our output will be like this:
+// "internetHealthMetrics": {
+//     "status": "UP",
+//     "details": {
+//         "success code": "Active Internet Connection"
+//     }
+// }
 @Component
 public class InternetHealthMetrics implements HealthIndicator {
     @Override
