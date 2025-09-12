0305aee66ed67b0d0b8a5b68c862e0f8257447ce
Hassan Raza <hassan.raza@redmath.com>
2025-09-12 17:29:23 +0500

    a

--- 14 Spring Security/06) Spring Security using Keycloak/keycloak/src/main/java/com/example/keycloak/controller/EmployeeController.java
+++ 14 Spring Security/06) Spring Security using Keycloak/keycloak/src/main/java/com/example/keycloak/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.example.keycloak.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//https://developers.redhat.com/articles/2023/07/24/how-integrate-spring-boot-3-spring-security-and-keycloak#install_keycloak
+
 @RestController
 @RequestMapping("/emp/v1")
 public class EmployeeController {
