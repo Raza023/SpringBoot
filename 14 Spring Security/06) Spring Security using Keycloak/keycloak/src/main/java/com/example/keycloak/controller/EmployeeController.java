31981f9b6671fcb358857d41e850f6327eb0bc40
Hassan Raza <hassan.raza@redmath.com>
2025-09-12 17:27:37 +0500

    key cloak started

---
+++ 14 Spring Security/06) Spring Security using Keycloak/keycloak/src/main/java/com/example/keycloak/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.example.keycloak.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp/v1")
+public class EmployeeController {
+
+}
