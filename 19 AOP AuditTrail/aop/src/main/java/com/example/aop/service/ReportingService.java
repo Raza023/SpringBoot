a5ade3db4405ad711c40f034879db975231177de
Hassan Raza <hassan.raza@redmath.com>
2026-03-30 12:16:24 +0500

    minor change in aop

--- 19 AOP AuditTrail/aop/src/main/java/com/example/aop/service/ReportingService.java
+++ 19 AOP AuditTrail/aop/src/main/java/com/example/aop/service/ReportingService.java
@@ -14,21 +14,25 @@ public class ReportingService {
     private final ReportingInfoRepository reportingInfoRepository;
 
     /**
-     * Updates a report for the given user.
+     * Updates a report for the given user by saving a new ReportingInfo object. The updated report will have the given
+     * user's email address, an amount of 100, and credit set to true and debit set to false. The account number will be
+     * set to "1234567890".
      *
      * @param email The email address of the user to update the report for.
      */
     @AuditTrail(action = "UPDATE_REPORT")
     public void updateReportByUser(String email) {
-        System.out.println("Updating report for: " + email);
+        // Create a new ReportingInfo object with the given email address and default values
+        ReportingInfo reportingInfo = ReportingInfo.builder()
+                .email(email)
+                .amount(new BigDecimal(100))
+                .credit(true)
+                .debit(false)
+                .accountNo("1234567890")
+                .build();
 
-        ReportingInfo reportingInfo = new ReportingInfo();
-        reportingInfo.setEmail(email);
-        reportingInfo.setAmount(new BigDecimal(100));
-        reportingInfo.setCredit(true);
-        reportingInfo.setDebit(false);
-        reportingInfo.setAccountNo("1234567890");
+        // Save the new ReportingInfo object
         reportingInfoRepository.save(reportingInfo);
-
     }
+
 }
