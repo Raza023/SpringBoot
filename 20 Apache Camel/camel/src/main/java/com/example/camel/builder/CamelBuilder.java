6f2b44804304c2df365b9c251d1edbe147d7c399
Hassan Raza <hassan.raza@redmath.com>
2026-07-01 12:22:57 +0500

    refactoring in apache camel

--- 20 Apache Camel/camel/src/main/java/com/example/camel/builder/CamelBuilder.java
+++ 20 Apache Camel/camel/src/main/java/com/example/camel/builder/CamelBuilder.java
@@ -20,17 +20,30 @@ public class CamelBuilder extends RouteBuilder {
         //copyAllFilesFromOneDirectoryToAnother();
 
         //Moving files from input to output directory using Apache Camel and keeps the file in the input>.camel dir
-        from("file:20 Apache Camel/camel/input")
-                .log("Moving file: ${header.CamelFileName}")
-                .to("file:20 Apache Camel/camel/output")
-                .log("Moved file: ${header.CamelFileName}");
-
+        moveFilesToOtherFolderWhileKeepingTheFilesInHiddenFolder();
     }
 
+    /**
+     * Copy all files from one directory to another. This method defines a Camel route that reads files from the "input"
+     * directory and copies them to the "output" directory. The "noop=true" option ensures that the original files are
+     * not deleted after processing.
+     */
     private void copyAllFilesFromOneDirectoryToAnother() {
         from("file:20 Apache Camel/camel/input?noop=true")
                 .log("Processing file: ${header.CamelFileName}")
                 .to("file:20 Apache Camel/camel/output")
                 .log("Copied file: ${header.CamelFileName}");
+    }
+
+    /**
+     * Move files from the input directory to the output directory while keeping the original files in a hidden folder.
+     * This method defines a Camel route that reads files from the "input" directory, moves them to the "output"
+     * directory, and keeps the original files in a hidden ".camel" folder within the input directory.
+     */
+    private void moveFilesToOtherFolderWhileKeepingTheFilesInHiddenFolder() {
+        from("file:20 Apache Camel/camel/input")
+                .log("Moving file: ${header.CamelFileName}")
+                .to("file:20 Apache Camel/camel/output")
+                .log("Moved file: ${header.CamelFileName}");
     }
 }
